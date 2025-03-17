eb97b2664b50f4e9ed2c00de07d1addb428c4c55
Marko Nedzbala <marko.nedzbala@gmail.com>
2025-03-16 22:32:51 -0400

    more reading

--- rediscovering javascript/Part02/part02.js
+++ rediscovering javascript/Part02/part02.js
@@ -2,10 +2,22 @@
 // Chapter04
 console.log('Chapter04');
 
+const names = ['Sara', 'Jake', 'Pete', 'Mark', 'Jill'];
+for(const name of names) {
+    console.log(name)
+}
 
+console.log();
 
+for(const entry of names.entries()) {
+    console.log(entry);
+}
 
+console.log();
 
+for(const [i, name] of names.entries()) {
+    console.log(i + '--' + name);
+}
 
 
 
