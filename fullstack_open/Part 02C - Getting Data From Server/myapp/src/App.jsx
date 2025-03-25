c82faaa912f8270bad570a980ffb3199ab1db150
Marko Nedzbala <marko.nedzbala@gmail.com>
2025-03-25 11:28:05 -0400

    full stack caught up

--- fullstack_open/Part 02C - Getting Data From Server/myapp/src/App.jsx
+++ fullstack_open/Part 02C - Getting Data From Server/myapp/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
+  const [stocks, setStocks] = useState([]);
+
   const hook = () => {
     console.log('effect');
     axios
@@ -18,18 +20,30 @@ function App() {
       })
   };
 
-  // useEffect(hook, []);
+  useEffect(hook, []);
 
-  const hook2 = () => {
-    axios
-      .get('https://localhost:3001/numbers')
-      .then(response => {
-        setNotes(response.data);
-      });
-  }
-  useEffect(hook2, []);
+  // const hook2 = () => {
+  //   axios
+  //     .get('https://localhost:3001/numbers')
+  //     .then(response => {
+  //       setNotes(response.data);
+  //     });
+  // }
+  // useEffect(hook2, []);
 
   console.log('render', notes.length, 'notes');
+
+  const hook3 = () => {
+    axios
+      .get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+      .then(response => {
+        setStocks(response.data);
+        console.log(response);
+      });
+  }
+
+  // useEffect(hook3, []);
+  // console.log('Stocks', stocks.length);
   
 
   return (
